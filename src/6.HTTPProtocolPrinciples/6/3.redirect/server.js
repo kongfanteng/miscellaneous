3d8461ca9adebf97af83899b366d6b201e3e9fb0
非凡腾达 <18210591009@163.com>
2024-06-14 11:43:36 +0800

    fix: HTTPProtocolPrinciples

--- src/6.HTTPProtocolPrinciples/6/3.redirect/server.js
+++ src/6.HTTPProtocolPrinciples/6/3.redirect/server.js
@@ -1,19 +1,22 @@
 const http = require('http')
 const fs = require('fs')
-const zib = require('zlib')
 
 http
   .createServer((req, res) => {
-    console.log(req.headers.host);
     if (req.url === '/') {
+      res.writeHead(302, {
+        Location: '/new',
+      })
+      res.end('')
+    }
+    if (req.url === '/new') {
       const data = fs.readFileSync('test.html', 'utf-8')
       res.writeHead(200, {
         'Content-Type': 'text/html',
-        'Content-Encoding': 'gzip',
       })
-      res.end(zib.gzipSync(data))
+      res.end(data)
     }
   })
-  .listen(8889, () => {
-    console.log('1.server is running 8889')
+  .listen(8888, () => {
+    console.log('1.server is running 8888')
   })
